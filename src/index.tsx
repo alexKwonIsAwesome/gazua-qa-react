a4fdc3b7692fb87b46c146791fb020ed662e06fd
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-15 16:47:49 +0900

    add styled-component and some styling rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import './global-styles';
 import registerServiceWorker from './registerServiceWorker';
 import App from './views/App';
 
