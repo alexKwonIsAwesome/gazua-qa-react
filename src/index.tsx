acd659b65abdf3e940f364c2dac46073bca8f88b
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-15 17:03:12 +0900

    add react-router-dom and config router in the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom'
 import './global-styles';
 import registerServiceWorker from './registerServiceWorker';
 import App from './views/App';
 
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
+import {
+  Route,
+} from 'react-router-dom'
+
+const Comp = () => <div>Hello!</div>
 
 class App extends React.Component {
   public render() {
     return (
       <div>
-        Hello World!
+        <Route
+          exact={true}
+          path="/"
+          component={Comp}
+        />
       </div>
     );
   }
