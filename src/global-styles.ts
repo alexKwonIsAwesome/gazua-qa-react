aec0db494bb908f97a2059f68a92e7ca21d63fa7
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-16 15:16:42 +0900

    add global styling rules

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -5,6 +5,16 @@ injectGlobal`
     margin: 0;
     padding: 0;
     font-size: 62.5%;
-    font-family: 'NanumSquare', sans-serif;
+    font-family: 'NanumSquare', sans-serif !important;
+    background-color: #f5f5f5;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: antialiased;
+    -o-font-smoothing: antialiased;
+    font-smoothing: antialiased;
+  }
+
+  button:focus {
+    outline: none;
   }
 `;
