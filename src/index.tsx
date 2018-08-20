53beb4b7233f08e7484224e27364de31ca6404fe
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-20 15:13:39 +0900

    add apollo-client related libraries
    Installed apollo-boost, graphql, react-apollo and created new client object on index.ts with ApolloClient class. Set the server uri as config and passed client object to ApolloProvider component as a HOC on top of the root component.

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,23 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom'
 import 'antd/dist/antd.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './global-styles';
 import registerServiceWorker from './registerServiceWorker';
 import App from './views/App';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
